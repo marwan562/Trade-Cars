2ab9477c5cf0bdc9bc71d4cb00622967e36d265a
marwan.hassan <mixing.gamer546@gmail.com>
2024-03-18 14:22:36 +0200

    Deploing

--- src/app/_product/ProductList.jsx
+++ src/app/_product/ProductList.jsx
@@ -1,13 +1,15 @@
-import ProductItem from "./ProductItem"
+import ProductItem from "./ProductItem";
 
-function ProductList({productList}) {
- 
+function ProductList({ productList }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6  md:items-center  gap-5">
-        {productList.map(item=>  <div key={item.id}><ProductItem  product={item} /></div>
-)}
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  mb-10  md:items-center  gap-5">
+      {productList.map((item) => (
+        <div key={item.id}>
+          <ProductItem product={item} />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default ProductList
+export default ProductList;
